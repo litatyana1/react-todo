e9c81fa75c483b3ffeb4ba647c6e1a13f8502591
Tatyana <litatyana82@yahoo.com>
2023-10-02 13:21:05 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function TodoList() {
+  const todoList = [
+    { id: 1, title: "Complete assignment 1" },
+    { id: 2, title: "Read a book" },
+    { id: 3, title: "Go to the gym" }
+  ];
+
+  const listItems = todoList.map((item) => (
+    <li key={item.id}>{item.title}</li>
+  ));
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <TodoList /> {}
+    </div>
+  );
+}
+
+export default App;
