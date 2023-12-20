d09be0b87b9e0f55eba3c5e20b435debbcc1977a
Tatyana <litatyana82@yahoo.com>
2023-12-20 16:06:04 -0500

    lesson_1_9

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
@@ -37,6 +38,16 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
+
   const addTodo = async (newTodo) => {
     try {
       const options = {
@@ -46,9 +57,7 @@ function App() {
           Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
         },
         body: JSON.stringify({
-          fields: {
-            title: newTodo.title,
-          },
+          fields: { title: newTodo.title },
         }),
       };
 
@@ -61,41 +70,57 @@ function App() {
       }
 
       const addedTodo = await response.json();
-      setTodoList((prevTodos) => [...prevTodos, { id: addedTodo.id, title: addedTodo.fields.title }]);
+
+      setTodoList([...todoList, { title: addedTodo.fields.title, id: addedTodo.id }]);
     } catch (error) {
       console.error(error.message);
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const removeTodo = async (id) => {
+    try {
+      // Add logic to remove a todo from the Airtable API (DELETE method)
+      // ...
 
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+      // For now, let's update the local state without API integration
+      const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+      setTodoList(updatedTodoList);
+    } catch (error) {
+      console.error(error.message);
     }
-  }, [todoList, isLoading]);
-
-  const removeTodo = (id) => {
-    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(updatedTodoList);
   };
 
   return (
-    <>
-      <header>
-        <h1>Todo List</h1>
-      </header>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-          <AddTodoForm onAddTodo={addTodo} />
-        </>
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <header>
+                <h1>Todo List</h1>
+              </header>
+              {isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                <>
+                  <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+                  <AddTodoForm onAddTodo={addTodo} />
+                </>
+              )}
+            </>
+          }
+        />
+        <Route
+          path="/new"
+          element={
+            <>
+              <h1>New Todo List</h1>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
